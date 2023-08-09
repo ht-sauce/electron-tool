03188e59859c41ff686b5bc2c1386efafd4f92d7
dht <603928588@qq.com>
2023-08-09 17:16:44 +0800

    提示信息处理

--- src/page/wiki/flow-chart/index.vue
+++ src/page/wiki/flow-chart/index.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="chart">
+    <TooltipTips />
     <div class="stencil-fef" ref="stencilRef"></div>
     <div class="flow-chart" ref="flowChartRef">流程图表</div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import TooltipTips from './tooltip-tips.vue'
 import { onMounted, ref } from 'vue'
 import { Graph } from '@antv/x6'
 import { Snapline } from '@antv/x6-plugin-snapline' // 对齐线
 import { Keyboard } from '@antv/x6-plugin-keyboard' // 快捷键
-import { Selection } from '@antv/x6-plugin-selection' // snapline
-import { Stencil } from '@antv/x6-plugin-stencil'
+import { Selection } from '@antv/x6-plugin-selection' // 框选
+import { Stencil } from '@antv/x6-plugin-stencil' // 面板
 
 const flowChartRef = ref<HTMLDivElement>(null)
 const stencilRef = ref<HTMLDivElement>(null)
@@ -29,8 +31,15 @@ function initGraph() {
     container: div,
     autoResize: true,
     // 缩放与平移
-    panning: true,
-    mousewheel: true,
+    panning: {
+      enabled: true,
+      modifiers: 'alt',
+    },
+    // 缩放
+    mousewheel: {
+      enabled: true,
+      modifiers: ['ctrl', 'meta'],
+    },
     grid: {
       visible: true,
       type: 'doubleMesh',
@@ -49,7 +58,57 @@ function initGraph() {
   })
   graph.use(new Snapline({ enabled: true }))
   graph.use(new Keyboard({ enabled: true }))
-  graph.use(new Selection({ enabled: true }))
+  graph.use(
+    new Selection({ enabled: true, multiple: true, rubberband: true, movable: true, showNodeSelectionBox: true }),
+  )
+  const commonAttrs = {
+    body: {
+      fill: '#fff',
+      stroke: '#8f8f8f',
+      strokeWidth: 1,
+    },
+  }
+  const n1 = graph.createNode({
+    shape: 'rect',
+    x: 40,
+    y: 40,
+    width: 80,
+    height: 40,
+    label: 'rect',
+    attrs: commonAttrs,
+  })
+
+  const n2 = graph.createNode({
+    shape: 'circle',
+    x: 180,
+    y: 40,
+    width: 40,
+    height: 40,
+    label: 'circle',
+    attrs: commonAttrs,
+  })
+
+  const n3 = graph.createNode({
+    shape: 'ellipse',
+    x: 280,
+    y: 40,
+    width: 80,
+    height: 40,
+    label: 'ellipse',
+    attrs: commonAttrs,
+  })
+
+  const n4 = graph.createNode({
+    shape: 'path',
+    x: 420,
+    y: 40,
+    width: 40,
+    height: 40,
+    // https://www.svgrepo.com/svg/13653/like
+    path: 'M24.85,10.126c2.018-4.783,6.628-8.125,11.99-8.125c7.223,0,12.425,6.179,13.079,13.543c0,0,0.353,1.828-0.424,5.119c-1.058,4.482-3.545,8.464-6.898,11.503L24.85,48L7.402,32.165c-3.353-3.038-5.84-7.021-6.898-11.503c-0.777-3.291-0.424-5.119-0.424-5.119C0.734,8.179,5.936,2,13.159,2C18.522,2,22.832,5.343,24.85,10.126z',
+    attrs: commonAttrs,
+    label: 'path',
+  })
 
   const stencil = new Stencil({
     target: graph,
@@ -59,6 +118,7 @@ function initGraph() {
     notFoundText: '未找到',
     groups: [],
   })
+  stencil.load([n1, n2, n3, n4])
   stencilRef.value.appendChild(stencil.container)
 }
 </script>
---
+++ src/page/wiki/flow-chart/tooltip-tips.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-tooltip placement="left" effect="light" trigger="click">
+    <template #content>
+      <pre>
+1、按住ctrl键，滚动鼠标滚轮，可以缩放画布
+2、按住Alt键，可以拖动画布
+3、按住鼠标左键，可以批量选中节点</pre
+      >
+    </template>
+    <el-button class="tooltip-tips" :icon="QuestionFilled" link></el-button>
+  </el-tooltip>
+</template>
+<script setup lang="ts">
+import { QuestionFilled } from '@element-plus/icons-vue'
+</script>
+
+<style scoped lang="scss">
+@use '@/theme/vars/function.scss' as *;
+.tooltip-tips {
+  position: absolute;
+  right: 0;
+  z-index: 1;
+  color: GetVar('info0');
+  font-size: 24px;
+  border-radius: 50%;
+  padding: 0;
+  width: 24px;
+  height: 24px;
+  &:hover {
+    color: GetVar('main-color5');
+    box-shadow: GetVar('shadow2');
+  }
+}
+</style>
